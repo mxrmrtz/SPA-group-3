f0efddcf7e6b09ed7c0e0ce665122001e32a6dba
Erik Friis <erik.friis@hyperisland.sem>
2023-01-06 16:13:04 +0100

    skeleton product style +add icons to product cards

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,14 +1,36 @@
-const ProductItem = ({productsData}) => {
-	return(
-		<li>
-			<div>
-			<img src={productsData.img} alt="drone"/>
-			<h2>{productsData.brand} {productsData.title}</h2>
-			<div>{productsData.units}{productsData.price}</div>
-			<div>{productsData.rating}</div>
+import React from "react";
+import heart from "../../assets/icons/heart-icon.svg";
+import add from "../../assets/icons/Add-icon.svg";
+import ProductItemCSS from "./ProductItem.module.css";
+const ProductItem = ({ productsData }) => {
+	return (
+		<li className={ProductItemCSS.productListItem}>
+			<div className={ProductItemCSS.productContainer}>
+				<div className={ProductItemCSS.iconsContainer}>
+					<button>
+						<img src={heart} alt="hert icon used to save an item fro later" />
+					</button>
+					<button>
+						<img src={add} alt="icon used to add item to shopping cart" />
+					</button>
+				</div>
+				<img
+					src={productsData.img}
+					alt="drone"
+					className={ProductItemCSS.productImage}
+				/>
+				<div>
+					<h2>
+						{productsData.brand} {productsData.title}
+					</h2>
+					<div>
+						{productsData.units}
+						{productsData.price}
+					</div>
+					<div>{productsData.rating}</div>
+				</div>
 			</div>
 		</li>
-
-	)
-}
-export default ProductItem
+	);
+};
+export default ProductItem;
